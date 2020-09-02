195ffd37eae986bd8d5930e7fb7bc5f78cc948ef
GabrielEncarnacion <70559975+GabrielEncarnacion@users.noreply.github.com>
2020-09-02 16:12:56 -0500

    Add files via upload

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("Hi, my name is Gabriel.")
+print("I´m 18 years old.")
+print("My senior year quote was \"Your focus determines your reality\"", "by Qui-Gonn Jinn.")
+print('My friends said it was the best quote of the year!')
